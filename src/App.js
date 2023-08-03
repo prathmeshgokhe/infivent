a87dcdc538e5f8947410b4e2aa0400c01bde3b6f
Prathmesh Gokhe <pkgokhe1969@gmail.com>
2023-08-03 22:45:17 +0530

    Vision mission fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Hero from "./components/Hero"
+import Visionmission from "./components/Visionmission"
 function App() {
   return (
     <>
       <div className='app'>
         <Hero />
+        <Visionmission />
       </div>
     </>
   );
