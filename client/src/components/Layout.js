9a38840fc7219b6b2eb6082ab2601602d32f8db6
Prathmesh Gokhe <pkgokhe1969@gmail.com>
2023-08-15 22:38:25 +0530

    updated

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -6,16 +6,10 @@ const Layout = ({ children }) => {
   return (
     <div>
       <Header />
-<<<<<<< HEAD
-      <main>{children}</main>
-      {/* <Footer /> */}
-    </>
-=======
       <main style={{ minHeight: "70vh" }}>{children}</main>
 
       <Footer />
     </div>
->>>>>>> f6d6a215709f581d1525e452474567227a6a7a09
   );
 };
 
--- client/src/pages/Aboutus.js
+++ client/src/pages/Aboutus.js
@@ -4,26 +4,12 @@ import Layout from "../components/Layout";
 import About from "../components/About";
 import MentorTeam from "../components/MentorTeam";
 const Aboutus = () => {
-<<<<<<< HEAD
     return (
-        <div className='aboutusmain'>
-            <Layout>
-                <Aboutuscontent />
-                <ContactForm />
-            </Layout>
-        </div>
-    )
-}
-
-export default Aboutus
-=======
-  return (
-    <Layout>
-      <About />
-      <MentorTeam />
-    </Layout>
-  );
+        <Layout>
+            <About />
+            <MentorTeam />
+        </Layout>
+    );
 };
 
 export default Aboutus;
->>>>>>> f6d6a215709f581d1525e452474567227a6a7a09
